62fe13a11f3ef84355aefc4dbe64eaf69cf29980
Nebra98 <bulicbrane561@gmail.com>
2023-03-08 22:21:04 +0100

    Created CreateCreateUser Seeder.php which sets three different users in the database

---
+++ database/seeders/CreateUserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+          ['name' => 'User',
+            'email' => 'user@mail.com',
+              'password' => bcrypt("password"),
+              'role' => 0
+          ],
+            ['name' => 'Editor',
+                'email' => 'editor@mail.com',
+                'password' => bcrypt("password"),
+                'role' => 1
+            ],
+            ['name' => 'Admin',
+                'email' => 'admin@mail.com',
+                'password' => bcrypt("password"),
+                'role' => 2
+            ]
+        ];
+
+        foreach ($users as $user){
+            User::create($user);
+        }
+
+    }
+}
